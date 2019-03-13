edea62ba546d4848c2bd2ae9fb87f98308797c43
Daniil <ddydeveloper@gmail.com>
2019-03-14 00:03:08 +0300

    fixed compose flow

--- docker/meetings/MeetingsApi/Program.cs
+++ docker/meetings/MeetingsApi/Program.cs
@@ -12,7 +12,6 @@ namespace MeetingsApi
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .UseUrls("http://localhost:5002");
+                .UseStartup<Startup>();
     }
 }
--- docker/users/UsersApi/Program.cs
+++ docker/users/UsersApi/Program.cs
@@ -12,7 +12,6 @@ namespace UsersApi
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .UseUrls("http://localhost:5001");
+                .UseStartup<Startup>();
     }
 }
