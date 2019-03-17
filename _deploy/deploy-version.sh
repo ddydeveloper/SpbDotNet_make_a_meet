b1c839cef1886f44844dea5a97ff7dce7de6f493
Daniil <ddydeveloper@gmail.com>
2019-03-17 20:22:47 +0300

    fixed path

--- _deploy/deploy-version.sh
+++ _deploy/deploy-version.sh
@@ -1,8 +1,8 @@
 # Build and tag images
-docker build -t ddydeveloper/make-a-meet-client:$SHA       -f ./3_docker/client-app/Dockerfile ./3_docker/client-app
-docker build -t ddydeveloper/make-a-meet-users-api:$SHA    -f ./3_docker/users/Dockerfile      ./3_docker/users
-docker build -t ddydeveloper/make-a-meet-meetings-api:$SHA -f ./3_docker/meetings/Dockerfile   ./3_docker/meetings
-docker build -t ddydeveloper/make-a-meet-database:$SHA     -f ./3_docker/database/Dockerfile   ./3_docker/database
+docker build -t ddydeveloper/make-a-meet-client:$SHA       -f ../3_docker/client-app/Dockerfile ../3_docker/client-app
+docker build -t ddydeveloper/make-a-meet-users-api:$SHA    -f ../3_docker/users/Dockerfile      ../3_docker/users
+docker build -t ddydeveloper/make-a-meet-meetings-api:$SHA -f ../3_docker/meetings/Dockerfile   ../3_docker/meetings
+docker build -t ddydeveloper/make-a-meet-database:$SHA     -f ../3_docker/database/Dockerfile   ../3_docker/database
 
 # Push images to registry
 docker push ddydeveloper/make-a-meet-client:$SHA
