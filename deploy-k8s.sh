65cbb3374eb394b5dd3d1a1e89d8f20fd785bd3c
Daniil <ddydeveloper@gmail.com>
2019-03-17 22:41:02 +0300

    only prod k8s

--- deploy-k8s.sh
+++ deploy-k8s.sh
@@ -3,7 +3,7 @@ kubectl delete secret database-secret
 kubectl create secret generic database-secret --from-literal SA_PASSWORD="P@ssw0rd" --from-literal USERS_DB="Server=database-cluster-ip-service;DataBase=Users;User Id=sa;Password=P@ssw0rd;Connection Timeout=30;" --from-literal MEETINGS_DB="Server=database-cluster-ip-service;DataBase=Meetings;User Id=sa;Password=P@ssw0rd;Connection Timeout=30;"
 
 # Apply workloads and services
-kubectl apply -f kubernetes-prod
+kubectl apply -f 5_kubernetes
 kubectl set image deployments/client-deployment       client=ddydeveloper/make-a-meet-client:$SHA
 kubectl set image deployments/users-api-deployment    users-api=ddydeveloper/make-a-meet-users-api:$SHA
 kubectl set image deployments/meetings-api-deployment meetings-api=ddydeveloper/make-a-meet-meetings-api:$SHA
