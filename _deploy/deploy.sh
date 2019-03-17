6524720919adc773ff1d16eb602567a1d0fafdc0
Daniil <ddydeveloper@gmail.com>
2019-03-17 21:31:08 +0300

    try

--- _deploy/deploy.sh
+++ _deploy/deploy.sh
@@ -1,8 +1,8 @@
 # Build and tag images
-docker build -t ddydeveloper/make-a-meet-client:latest       -f ../4_docker/client-app/Dockerfile ../4_docker/client-app
-docker build -t ddydeveloper/make-a-meet-users-api:latest    -f ../4_docker/users/Dockerfile      ../4_docker/users
-docker build -t ddydeveloper/make-a-meet-meetings-api:latest -f ../4_docker/meetings/Dockerfile   ../4_docker/meetings
-docker build -t ddydeveloper/make-a-meet-database:latest     -f ../4_docker/database/Dockerfile   ../4_docker/database
+docker build -t ddydeveloper/make-a-meet-client:latest       -f ./4_docker/client-app/Dockerfile ./4_docker/client-app
+docker build -t ddydeveloper/make-a-meet-users-api:latest    -f ./4_docker/users/Dockerfile      ./4_docker/users
+docker build -t ddydeveloper/make-a-meet-meetings-api:latest -f ./4_docker/meetings/Dockerfile   ./4_docker/meetings
+docker build -t ddydeveloper/make-a-meet-database:latest     -f ./4_docker/database/Dockerfile   ./4_docker/database
 
 # Push images to registry
 docker push ddydeveloper/make-a-meet-client:latest
