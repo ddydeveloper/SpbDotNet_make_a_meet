15b4d1e4ac5e0d472a0506063653c96834ffcf67
Daniil <ddydeveloper@gmail.com>
2019-03-18 14:22:28 +0300

    Update MeetingsController.cs

--- 4_docker/meetings/MeetingsApi/Controllers/MeetingsController.cs
+++ 4_docker/meetings/MeetingsApi/Controllers/MeetingsController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MeetingsApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/meetings/[controller]")]
     [ApiController]
     public class MeetingsController : ControllerBase
     {
